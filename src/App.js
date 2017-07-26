ecc683643b5963859309502f85672c3827c18f9d
Justin He <hejustin@usc.edu>
2017-07-26 15:10:34 -0700

    some quick improvements in the minimal freetime i have

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ class App extends Component {
     return (
       <div>
         <Splash />
+        <div className="seperator"> </div>
         <div className="row">
           <Track text="Future of Los Angeles" column="col-4" textcolor="#FFFFFF" bgcolor="#0BDDEF" />
           <Track text="Health + Active Lifestyle" column="col-4" textcolor="#FFFA00" bgcolor="#F9728D" />
           <Track text="Art + Culture" column="col-4" textcolor="#0BDDEF" bgcolor="#FFFA00" />
         </div>
+        <div className="seperator"> </div>
+        <Schedule />
       </div>
     );
   }
@@ -29,11 +32,31 @@ class Splash extends Component {
   }
 }
 
+class Schedule extends Component {
+  render() {
+    return (
+      <div className="schedule">
+        <h1>Schedule here. CSS not yet implemented.</h1>
+        <h3>Placeholder text.</h3>
+      </div>
+    )
+  }
+}
+
+class Event extends Component {
+
+}
+
 class Track extends Component {
   render() {
     return (
       <div style={{color: this.props.textcolor, backgroundColor: this.props.bgcolor}} className={["track", this.props.column].join(' ')}>
         <h1>{this.props.text}</h1>
+        <p> I don't know what to put here so this is Filler text.
+        I don't know what to put here so this is Filler text.
+        I don't know what to put here so this is Filler text.
+        I don't know what to put here so this is Filler text.
+        I don't know what to put here so this is Filler text.</p>
       </div>
     )
   }
